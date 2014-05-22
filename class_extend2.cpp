677ae1e62e2fac5e128a6875bc94ccc23582831a
1oading <458217992@qq.com>
2014-05-22 15:01:35 +0800

    Update class_extend2.cpp

--- class_extend2.cpp
+++ class_extend2.cpp
@@ -3,6 +3,11 @@
  *
  * 20
  * 10
+ * 20
+ * 10
+ * 20
+ * 10
+ * 0
  *
  */
 
@@ -16,6 +21,14 @@ public:
 	void set_values(int a, int b) {
 		width = a;
 		height = b;
+	}
+	
+	// virtual member
+	// after added to the father class,
+	// the pointer of father class can use
+	// the area function
+	virtual int area(void) {
+		return 0;
 	}
 };
 
@@ -49,5 +62,28 @@ int main() {
 	trgl.set_values(4, 5);
 	rect.output(rect.area());
 	trgl.output(trgl.area());
+	
+	CRectangle rect2;
+	CTriangle trgl2;
+	CPolygon* ppoly1 = &rect2;
+	CPolygon* ppoly2 = &trgl2;
+	ppoly1->set_values(4, 5);
+	ppoly2->set_values(4, 5);
+	cout << rect2.area() << endl;
+	cout << trgl2.area() << endl;
+	
+	CRectangle rect3;
+	CTriangle trgl3;
+	CPolygon poly;
+	ppoly1 = &rect3;
+	ppoly2 = &trgl3;
+	CPolygon* ppoly3 = &poly;
+	ppoly1->set_values(4, 5);
+	ppoly2->set_values(4, 5);
+	ppoly3->set_values(4, 5);
+	cout << ppoly1->area() << endl;
+	cout << ppoly2->area() << endl;
+	cout << ppoly3->area() << endl;
+	
 	return 0;
 }
